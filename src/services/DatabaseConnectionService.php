caa2ece9118627b0666f2bce3e168242fb63a2a2
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-04 09:40:21 +0000

    created gitignore, folder structure and connect to database service

---
+++ src/services/DatabaseConnectionService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+class DatabaseConnectionService
+{
+    public function connect(): PDO
+    {
+        $db = new PDO('mysql:host=DB;dbname=solo_blog_project', 'root', 'password');
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $db;
+    }
+}
