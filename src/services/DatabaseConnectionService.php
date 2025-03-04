21400d5dbf99ce40c22be756683f375167c7531c
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-04 09:43:03 +0000

    added static to public method

--- src/services/DatabaseConnectionService.php
+++ src/services/DatabaseConnectionService.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 class DatabaseConnectionService
 {
-    public function connect(): PDO
+    static public function connect(): PDO
     {
         $db = new PDO('mysql:host=DB;dbname=solo_blog_project', 'root', 'password');
         $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
