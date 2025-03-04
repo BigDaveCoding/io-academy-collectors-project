4789cf540d64b689490b0f70ac0d253f838fb088
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-04 14:18:52 +0000

    login validation working

--- login.php
+++ login.php
@@ -5,20 +5,15 @@ require_once 'src/services/LoginFormService.php';
 require_once 'src/services/DatabaseConnectionService.php';
 require_once 'src/services/NavBarService.php';
 require_once 'src/models/UsersModel.php';
+require_once 'src/services/LoginValidationService.php';
 
 $db = DatabaseConnectionService::connect();
 $usersModel = new UsersModel($db);
 
-echo "<pre>";
-
-if (isset($_POST['login'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $user = $usersModel->searchByUsername($username);
-    var_dump($user);
+if(isset($_POST['login'])){
+    $valid = LoginValidationService::validateLogin($db, $usersModel);
 }
 
-echo "</pre>";
 ?>
 
 <!doctype html>
@@ -29,13 +24,15 @@ echo "</pre>";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
-
     <title>Document</title>
 </head>
 <body>
     <?php
         echo NavBarService::display($_SESSION['loggedIn']);
-        echo LoginFormService::displayForm()
+        echo LoginFormService::displayForm();
+        if (!$valid){
+            echo LoginValidationService::errorMessage();
+        }
     ?>
 </body>
 </html>
