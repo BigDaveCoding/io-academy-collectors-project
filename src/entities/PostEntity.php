6137053d32656a3bdea6e5611e9156467d645d88
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-04 15:19:43 +0000

    had to add a public constructor to test service

--- src/entities/PostEntity.php
+++ src/entities/PostEntity.php
@@ -13,6 +13,31 @@ class PostEntity
     private int $dislikes;
     private int $category_2;
 
+    /**
+     * @param int $id
+     * @param string $title
+     * @param string $content
+     * @param string $category_id
+     * @param string $username
+     * @param string $date_time
+     * @param int $likes
+     * @param int $dislikes
+     * @param int $category_2
+     */
+    public function __construct(int $id = 0, string $title = '', string $content = '', string $category_id = '', string $username = '', string $date_time = '', int $likes = 0,  int $dislikes = 0, int $category_2 = 0)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->content = $content;
+        $this->category_id = $category_id;
+        $this->username = $username;
+        $this->date_time = $date_time;
+        $this->likes = $likes;
+        $this->dislikes = $dislikes;
+        $this->category_2 = $category_2;
+    }
+
+
     public function getId(): int
     {
         return $this->id;
