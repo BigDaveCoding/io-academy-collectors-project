43dcba1670b9ffadeaf1dffa56cedebce7a2fa46
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-04 10:49:24 +0000

    editied query to get title, content, date_teim and username. Using a JOIN to turn user_id into the associated username in users table.

--- src/models/PostsModel.php
+++ src/models/PostsModel.php
@@ -13,7 +13,10 @@ class PostsModel
 
     public function getAllPosts(): array
     {
-        $query = $this->db->prepare("SELECT * FROM `posts` ORDER BY `date_time` DESC");
+        $query = $this->db->prepare("SELECT `posts`.`title`, `posts`.`content`, `posts`.`date_time`, `users`.`username` FROM `posts`
+                                            JOIN `users`
+	                                            ON `posts`.`user_id` = `users`.`id`
+                                            ORDER BY `date_time` DESC;");
         $query->setFetchMode(PDO::FETCH_CLASS, PostEntity::class);
         if ($query->execute()){
             return $query->fetchAll();
