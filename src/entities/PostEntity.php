e0adde51785a30cd66c9505634a233affdba57fe
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-04 10:14:21 +0000

    created getters for post entity

--- src/entities/PostEntity.php
+++ src/entities/PostEntity.php
@@ -12,4 +12,51 @@ class PostEntity
     private int $likes;
     private int $dislikes;
     private int $category_2;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function getCategoryId(): string
+    {
+        return $this->category_id;
+    }
+
+    public function getUserId(): string
+    {
+        return $this->user_id;
+    }
+
+    public function getDateTime(): string
+    {
+        return $this->date_time;
+    }
+
+    public function getLikes(): int
+    {
+        return $this->likes;
+    }
+
+    public function getDislikes(): int
+    {
+        return $this->dislikes;
+    }
+
+    public function getCategory2(): int
+    {
+        return $this->category_2;
+    }
+
+
 }
