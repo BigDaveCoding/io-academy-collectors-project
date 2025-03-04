3626c9af42342bfe0eb8da39489e80a95ae4d51c
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-04 10:02:13 +0000

    created postsmodel and testing getallposts function in model

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+declare(strict_types=1);
+require_once 'src/services/DatabaseConnectionService.php';
+require_once 'src/models/PostsModel.php';
+
+$db = DatabaseConnectionService::connect();
+
+$postsModel = new PostsModel($db);
+
+echo "<pre>";
+var_dump($postsModel);
+var_dump($postsModel->getAllPosts());
