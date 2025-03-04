42404136bb2b01d7cecae04d801eef3636147b00
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-04 09:48:18 +0000

    created post entity which stores private variables. will add getters and setters when appropriate

---
+++ src/entities/PostEntity.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+class PostEntity
+{
+    private int $id;
+    private string $title;
+    private string $content;
+    private string $category_id;
+    private string $user_id;
+    private string $date_time;
+    private int $likes;
+    private int $dislikes;
+    private int $category_2;
+}
