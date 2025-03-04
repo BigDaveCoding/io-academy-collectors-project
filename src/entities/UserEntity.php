d08c3792fecf7d7069d05e97ca6e91c3fdb04ba8
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-04 12:53:06 +0000

    added getters to user Entity

--- src/entities/UserEntity.php
+++ src/entities/UserEntity.php
@@ -7,5 +7,25 @@ class UserEntity
     private string $password;
     private string $email;
 
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
 
 }
